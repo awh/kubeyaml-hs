fd322c85ed97a751b02489105d25e9287bab0122
Adam Harrison <harrisonadamw@gmail.com>
2020-03-13 07:50:36 +0000

    WIP

--- app/Main.hs
+++ app/Main.hs
@@ -1,90 +1,25 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE StandaloneDeriving #-}
 
 module Main where
 
 import qualified Data.ByteString.Lazy.Char8 as C
 import Text.Yaml.Reference (Code(..),Token(..),yaml)
-import Data.Map(Map)
-import Data.Map as Map
 
-import Text.Parsec.Prim
-import Text.Parsec.Combinator
-import Text.Parsec.Pos (newPos)
+import Text.Yaml.RoundTrip
+import Text.Parsec (runParser)
 
-import Data.Functor.Identity
+type Namespace = String
+type Kind = String
+type MetadataName = String
+type ContainerName = String
 
-import Lib
+findNode :: [String] -> Node -> Maybe Node
+findNode ns node =
 
-deriving instance Eq Token
-deriving instance Ord Code
-deriving instance Ord Token
-
-data Document = Document Node
-    deriving (Show, Eq)
-
-data Node = Scalar Token | Mapping (Map Node Node) | Sequence [Node]
-    deriving (Show, Eq, Ord)
-
-satisfy :: Stream s Identity Token => (Token -> Bool) -> ParsecT s u Identity Token
-satisfy f = token showTok posFromTok testTok
-    where
-        showTok t = show t
-        posFromTok t = newPos "" (tLine t) (tLineChar t)
-        testTok t = if f t then Just t else Nothing
-
-oneOf :: Stream s Identity Token => [Code] -> ParsecT s u Identity Token
-oneOf cs = satisfy (\t -> elem (tCode t) cs)
-
-noneOf :: Stream s Identity Token => [Code] -> ParsecT s u Identity Token
-noneOf cs = satisfy (\t -> not (elem (tCode t) cs))
-
-code :: Stream s Identity Token => Code -> ParsecT s u Identity Token
-code c = satisfy (\t -> c == tCode t) <?> show [c]
-
-notCode :: Stream s Identity Token => Code -> ParsecT s u Identity Token
-notCode c = satisfy (\t -> c /= tCode t) <?> show [c]
-
-bracket :: Stream s Identity Token => Code -> Code -> ParsecT s u Identity a -> ParsecT s u Identity a
-bracket startCode endCode contentParser = do
-    skipMany $ notCode startCode
-    between (code startCode) (code endCode) contentParser
-
-parseStream :: Stream s Identity Token => ParsecT s u Identity [Document]
-parseStream = many parseDocument
-
-parseDocument :: Stream s Identity Token => ParsecT s u Identity Document
-parseDocument = do
-    node <- bracket BeginDocument EndDocument parseNode
-    return $ Document node
-
-parseNode :: Stream s Identity Token => ParsecT s u Identity Node
-parseNode = do
-    bracket BeginNode EndNode (choice [parseScalar, parseSequence, parseMapping])
-
-parseScalar :: Stream s Identity Token => ParsecT s u Identity Node
-parseScalar = do
-    text <- bracket BeginScalar EndScalar (code Text)
-    return $ Scalar text
-
-parseSequence :: Stream s Identity Token => ParsecT s u Identity Node
-parseSequence = do
-    nodes <- bracket BeginSequence EndSequence (many1 parseNode)
-    return $ Sequence nodes
-
-parseMapping :: Stream s Identity Token => ParsecT s u Identity Node
-parseMapping = do
-    pairs <- bracket BeginMapping EndMapping (many1 parsePair)
-    return $ Mapping $ Map.fromList pairs
-
-parsePair :: Stream s Identity Token => ParsecT s u Identity (Node, Node)
-parsePair = do
-    bracket BeginPair EndPair contentParser
-    where
-        contentParser = do
-            key <- parseNode
-            value <- parseNode
-            return (key, value)
+find :: Namespace -> Kind -> MetadataName -> ContainerName -> Document -> Maybe Scalar
+find ns k mn cn d =
+    case d of
+        Mapping md -> undefined
+        _          -> Nothing 
 
 main :: IO ()
 main = do
---
+++ src/Text/Yaml/RoundTrip.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
+module Text.Yaml.RoundTrip (
+    Document(..),
+    Node(..),
+    parseStream
+    ) where
+
+import Text.Yaml.Reference (Code(..),Token(..),yaml)
+import Data.Map(Map)
+import Data.Map as Map
+
+import Text.Parsec.Prim
+import Text.Parsec.Combinator
+import Text.Parsec.Pos (newPos)
+
+import Data.Functor.Identity
+
+deriving instance Eq Token
+deriving instance Ord Code
+deriving instance Ord Token
+
+data Document = Document Node
+    deriving (Show, Eq)
+
+data Node = Scalar Token | Mapping (Map Node Node) | Sequence [Node]
+    deriving (Show, Eq, Ord)
+
+satisfy :: Stream s Identity Token => (Token -> Bool) -> ParsecT s u Identity Token
+satisfy f = token showTok posFromTok testTok
+    where
+        showTok t = show t
+        posFromTok t = newPos "" (tLine t) (tLineChar t)
+        testTok t = if f t then Just t else Nothing
+
+oneOf :: Stream s Identity Token => [Code] -> ParsecT s u Identity Token
+oneOf cs = satisfy (\t -> elem (tCode t) cs)
+
+noneOf :: Stream s Identity Token => [Code] -> ParsecT s u Identity Token
+noneOf cs = satisfy (\t -> not (elem (tCode t) cs))
+
+code :: Stream s Identity Token => Code -> ParsecT s u Identity Token
+code c = satisfy (\t -> c == tCode t) <?> show [c]
+
+notCode :: Stream s Identity Token => Code -> ParsecT s u Identity Token
+notCode c = satisfy (\t -> c /= tCode t) <?> show [c]
+
+ignoreCodes = [Meta, Break, Indicator, White, Indent, DirectivesEnd, BeginComment, EndComment]
+
+bracket :: Stream s Identity Token => Code -> Code -> ParsecT s u Identity a -> ParsecT s u Identity a
+bracket startCode endCode contentParser = do
+    skipMany $ oneOf ignoreCodes
+    between (code startCode) (code endCode) contentParser
+
+parseStream :: Stream s Identity Token => ParsecT s u Identity [Document]
+parseStream = many parseDocument
+
+parseDocument :: Stream s Identity Token => ParsecT s u Identity Document
+parseDocument = do
+    node <- bracket BeginDocument EndDocument parseNode
+    return $ Document node
+
+parseNode :: Stream s Identity Token => ParsecT s u Identity Node
+parseNode = do
+    node <- bracket BeginNode EndNode (choice [try parseScalar, try parseSequence, try parseMapping])
+    skipMany $ oneOf ignoreCodes
+    return node
+
+parseScalar :: Stream s Identity Token => ParsecT s u Identity Node
+parseScalar = do
+    text <- bracket BeginScalar EndScalar (code Text)
+    return $ Scalar text
+
+parseSequence :: Stream s Identity Token => ParsecT s u Identity Node
+parseSequence = do
+    nodes <- bracket BeginSequence EndSequence (many parseNode)
+    return $ Sequence nodes
+
+parseMapping :: Stream s Identity Token => ParsecT s u Identity Node
+parseMapping = do
+    pairs <- bracket BeginMapping EndMapping (many parsePair)
+    return $ Mapping $ Map.fromList pairs
+
+parsePair :: Stream s Identity Token => ParsecT s u Identity (Node, Node)
+parsePair = do
+    bracket BeginPair EndPair contentParser
+    where
+        contentParser = do
+            key <- parseNode
+            value <- parseNode
+            return (key, value)
